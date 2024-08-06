36e582b623df8b4543715f89c8d3023f2687d517
Daddy2054 <stivali-tinte-0w@icloud.com>
2024-08-06 21:09:15 +0200

    Initial Commit

---
+++ Code History/Code_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_HistoryApp.swift
+//  Code History
+//
+//  Created by Jean on 06/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Code_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
